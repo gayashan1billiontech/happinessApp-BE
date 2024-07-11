3154a471a7804b25fe2b7b86701038c7bdbbde83
“Madushan-Neelananda” <madushan.neelananda@1billiontech.com>
2024-07-11 14:05:47 +0530

    added directory logs

--- libs/dependencies.sh
+++ libs/dependencies.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+
 echo 'Installing Libs dependencies >>>'
+pwd
+
 npm install aws-sdk@2.1655.0 uuid@10.0.0 @hapi/joi@17.1.1 data-api-client@1.3.0 lodash@4.17.21 --silent
-npm install aws-sdk uuid  @hapi/joi  amazon-cognito-identity-js  moment aws-xray-sdk@3.9.0  data-api-client lodash --silent
+pwd
+
+npm install aws-sdk uuid @hapi/joi amazon-cognito-identity-js moment aws-xray-sdk@3.9.0 data-api-client lodash --silent
+pwd
+
 echo 'Installing Libs dependencies completed :)'
+pwd
