2c190f643ac6b987eec84d072349c68db112b6e0
gayashan1billiontech <gayashan.galagedara@1billiontech.com>
2024-07-13 13:54:27 +0530

    authorize apis for both platforms

--- notification-service/handler.js
+++ notification-service/handler.js
@@ -8,7 +8,17 @@ const hapinessHandler = {
         message: "Go Serverless v4! Your function executed successfully!",
       }),
     };
+  },
+
+  helloAdmin: async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Go Serverless v4! Your function executed successfully!",
+      }),
+    };
   }
+
 
 }
 
