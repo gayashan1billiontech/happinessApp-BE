f5924fb7a7776672ddf5e5182f9e7f870eca1077
gayashan1billiontech <gayashan.galagedara@1billiontech.com>
2024-07-13 06:10:07 +0530

    update admin user service

---
+++ admin-user-service/handler.js
@@ -0,0 +1,15 @@
+
+const hapinessHandler = {
+
+  hello: async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Go Serverless v4! Your function executed successfully!",
+      }),
+    };
+  }
+
+}
+
+module.exports = hapinessHandler;
